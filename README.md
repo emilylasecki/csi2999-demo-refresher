7183fced15d39715d4c5aa7347557145a6d22aa2
Emily Lasecki <emilylasecki@oakland.edu>
2024-12-30 17:16:25 -0500

    added first 2 button presses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # csi2999-demo-refresher
-script to add an entry to my csi2999 project to prevent the database from pausing
+
+This project visits my csi2999 project (or sophomore project) at https://csi-2999-project.onrender.com/ and creates an entry for a campsite reservation. We used supabase's free tier for our development which will pauses access to the database if there is no activity for 7 days. Running this script twice a week prevents the database from pausing and the website displaying an error page.
--- campground.py
+++ campground.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ActionChains
+import time
+from selenium.webdriver.common.by import By
 
 service = Service(executable_path="C:\Program Files\chromedriver-win32\chromedriver.exe")
 options = webdriver.ChromeOptions()
@@ -14,6 +16,18 @@ request = requests.get("https://csi-2999-project.onrender.com/reservations")
 print(requests)
 
 soup = BeautifulSoup(request.content, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
+
+button = driver.find_element("id", "dateRange")
+button.click()
+#time.sleep(5)
+
+soup = BeautifulSoup(request.content, 'html.parser')
+print(soup.prettify)
+
+nextmonth = driver.find_element(By.CLASS_NAME, "flatpickr-next-month")
+
+nextmonth.click()
+time.sleep(5)
 
 # ActionChains(options).click(element).perform()
